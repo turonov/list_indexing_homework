6f8870e8b5dae8772d84fccfef2b5e35a36331f4
turonov <turonov5400gmail.com>
2022-11-02 11:50:45 +0500

    06

--- List06.py
+++ List06.py
@@ -6,4 +6,12 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    a =len(list1)
+    i=0
+    while i<a:
+        if list1[i] == 1:
+            list1[i]=True
+        i+=1
+    return list1
+
+print(main([1,0,0,1]))
